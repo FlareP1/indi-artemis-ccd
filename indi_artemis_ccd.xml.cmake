bfba9c9006a466f703324d39897604520416c0a9
FlareP1 <chrishewitt35@hotmail.com>
2021-03-04 19:23:37 +0100

    Intial checkin prototype Artemis CCD controller

---
+++ indi_artemis_ccd.xml.cmake
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+   <devGroup group="CCDs">
+   <device label="Artemis CCD" mdpd="true" manufacturer="Artemis">>
+         <driver name="Artemis">indi_artemis_ccd</driver>
+         <version>@CDRIVER_VERSION_MAJOR@.@CDRIVER_VERSION_MINOR@</version>
+      </device>
+   </devGroup>
+</driversList>
+
